bf871dc97d3ec8026c2a96f8362ef5b550c9d8c4
nguylisa <lisan2205@gmail>
2020-09-09 12:24:38 +0200

    1

---
+++ src/my_project/control/GameControll.java
@@ -0,0 +1,14 @@
+package my_project.control;
+
+import KAGO_framework.control.ViewController;
+import KAGO_framework.model.abitur.datenstrukturen.BinarySearchTree;
+
+public class GameControll {
+    private ViewController vC;
+
+    public GameControll(ViewController viewControll){
+        vC = viewControll;
+    }
+
+
+}
