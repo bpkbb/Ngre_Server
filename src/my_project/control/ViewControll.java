58c702d4b30e9a0b6e766ce0a635a1591e578945
Lisa <lisan2205@gmail.com>
2020-09-10 19:03:43 +0200

    GUI Client

--- src/my_project/control/ViewControll.java
+++ src/my_project/control/ViewControll.java
@@ -1,11 +1,14 @@
 package my_project.control;
 
 import my_project.view.Anwenderinterface;
+import my_project.view.ClientInterface;
 
 public class ViewControll {
     private Anwenderinterface a;
+    private ClientInterface client;
 
     public ViewControll() {
         a = new Anwenderinterface();
+        client = new ClientInterface();
     }
 }
