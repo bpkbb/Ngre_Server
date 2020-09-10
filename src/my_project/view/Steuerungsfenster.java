bfe631b6b0ff1701bb8fd0e68e8f94ecccff0f44
Lisa <lisan2205@gmail.com>
2020-09-10 19:08:48 +0200

    Steuerungsfenster hat ein JFrame

--- src/my_project/view/Steuerungsfenster.java
+++ src/my_project/view/Steuerungsfenster.java
@@ -5,10 +5,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Steuerungsfenster {
+    private JFrame fenster;
     private JButton serverButton;
     private JButton clientButton;
+    private JPanel ASC;
 
     public Steuerungsfenster() {
+        fenster = new JFrame("Startfenster zu Ngre Server und Joli-Client");
+        fenster.setContentPane(ASC);
         serverButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
